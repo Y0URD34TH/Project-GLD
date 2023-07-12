fc98edf0310b9acf53acbce4ae2ef1fdc7cdfb48
jma <58450502+Y0URD34TH@users.noreply.github.com>
2023-07-12 15:48:00 -0300

    Update fitgirl-no-updatecheck.lua

--- Scripts/fitgirl-no-updatecheck.lua
+++ Scripts/fitgirl-no-updatecheck.lua
@@ -39,7 +39,7 @@ local function sanitizeMagnet(magnet)
     return magnet
 end
  local version = client.GetVersion()
- if version ~= "V1.00" then
+ if version ~= "V1.01" then
   Notifications.push_error("Lua Script", "Program is Outdated Please Update to use that Script")
 else
   Notifications.push_success("Lua Script", "fitgirl Script Loaded And Working")
