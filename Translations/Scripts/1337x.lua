fa670dd00c16a7ea8dc5a17fd55c2a555667eb18
jma <58450502+Y0URD34TH@users.noreply.github.com>
2023-07-28 14:33:24 -0300

    Updated Scripts and translations to V1.17
    updated scripts and new translations strings

---
+++ Translations/Scripts/1337x.lua
@@ -0,0 +1,147 @@
+--1.16
+local function checkVersion(str, comparison)
+    local serverversion = str:sub(3, 6)
+    return serverversion == comparison
+end
+
+local headers = {
+    ["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+}
+
+local version = "1.16"
+local githubversion = http.get("https://raw.githubusercontent.com/Y0URD34TH/Project-GLD/main/Scripts/1337x.lua", headers)
+
+if checkVersion(githubversion, version) then
+else
+    Notifications.push_warning("Script Outdated", "The Script Is Outdated Please Update")
+end
+
+local function endsWith(str, pattern)
+    return string.sub(str, -string.len(pattern)) == pattern
+end
+
+local function substituteRomanNumerals(gameName)
+    local romans = {
+        [" I"] = " 1",
+        [" II"] = " 2",
+        [" III"] = " 3",
+        [" IV"] = " 4",
+        [" V"] = " 5",
+        [" VI"] = " 6",
+        [" VII"] = " 7",
+        [" VIII"] = " 8",
+        [" IX"] = " 9",
+        [" X"] = " 10"
+    }
+
+    for numeral, substitution in pairs(romans) do
+        if endsWith(gameName, numeral) then
+            gameName = string.sub(gameName, 1, -string.len(numeral) - 1) .. substitution
+        end
+    end
+
+    return gameName
+end
+
+local regex = "<a href%s*=%s*\"(/torrent/[^\"]+)\""
+local magnetRegex = "href%s*=%s*\"(magnet:[^\"]+)\""
+
+local version = client.GetVersion()
+if version ~= "V1.17" then
+    Notifications.push_error("Lua Script", "Program is Outdated. Please Update to use this Script")
+else
+    Notifications.push_success("Lua Script", "1337x Script Loaded and Working")
+
+    menu.add_check_box("Disable Roman Numbers Conversion 1337x")
+    local romantonormalnumbers = true
+
+    local function checkboxcall()
+        romantonormalnumbers = not menu.get_bool("Disable Roman Numbers Conversion 1337x")
+    end
+
+    local function request1337x()
+        local gamename = game.getgamename()
+
+        if not gamename then
+            print("Error: Failed to retrieve game name.")
+            return
+        end
+
+        if romantonormalnumbers then
+            gamename = substituteRomanNumerals(gamename)
+        end
+
+        local urlrequest = "https://www.1377x.to/category-search/" .. tostring(gamename) .. "/Games/1/"
+        urlrequest = urlrequest:gsub(" ", "%%20")
+
+        local htmlContent = http.get(urlrequest, headers)
+        if not htmlContent then
+            print("Error: Failed to retrieve HTML content for URL: " .. urlrequest)
+            return
+        end
+
+        local results = {}
+        local searchResult -- Declare the searchResult variable outside the loop
+
+        for match in htmlContent:gmatch(regex) do
+            local url = "https://1377x.to" .. match
+            local torrentName = url:match("/([^/]+)/$")
+            if not torrentName then
+                print("Error: Failed to extract torrent name from URL: " .. url)
+                break
+            end
+
+            local htmlContent2 = http.get(url, headers)
+            if not htmlContent2 then
+                print("Error: Failed to retrieve HTML content for URL: " .. url)
+                break
+            end
+
+            searchResult = {
+                name = torrentName,
+                links = {},
+                ScriptName = "1337x"
+            }
+
+            for magnetMatch in htmlContent2:gmatch(magnetRegex) do
+                searchResult.links[#searchResult.links + 1] = {
+                    name = "Download",
+                    link = magnetMatch,
+                    addtodownloadlist = true
+                }
+                -- No need to continue the loop if a magnet link is found
+                break
+            end
+
+            if next(searchResult.links) == nil then
+                searchResult.links[#searchResult.links + 1] = {
+                    name = "Download",
+                    link = url
+                }
+            end
+
+            results[#results + 1] = searchResult
+        end
+
+        if next(results) ~= nil then
+            communication.receiveSearchResults(results)
+        else
+            print("No results found.")
+        end
+    end
+
+    client.add_callback("on_gameselected", request1337x) -- Callback when a game is selected in the menu
+    client.add_callback("on_present", checkboxcall) -- Callback when a game is selected in the menu
+end
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Translations/Scripts/prowlarr-no-updatecheck.lua
@@ -0,0 +1,82 @@
+local apikey = "fc99212ac3444bd5aac99556483e9e24" --YOUR API KEY GOES HERE
+local headers = {
+    ["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) ApplWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1788.0",
+    ["Accept"] = "application/json, text/javascript, */*; q=0.01",
+    ["Referer"] = "http://localhost:9696/search",
+    ["X-Api-Key"] = apikey,
+    ["X-Prowlarr-Client"] = "true",
+    ["X-Requested-With"] = "XMLHttpRequest",
+    ["Connection"] = "keep-alive",
+    ["Sec-Fetch-Mode"] = "cors",
+    ["Sec-Fetch-Dest"] = "empty",
+    ["Sec-Fetch-Site"] = "same-origin"
+}
+
+local function isMagnetLink(link)
+    return link and string.find(link, "magnet:")
+end
+
+local version = client.GetVersion()
+if version ~= "V1.17" then
+    Notifications.push_error("Lua Script", "Program is Outdated. Please Update to use this Script")
+else
+    Notifications.push_success("Lua Script", "prowlarr Script Loaded")
+    Notifications.push_warning("Prowlarr Script", "Be sure to put your API Key in the lua script!!!")
+
+    local function prowlarr()
+        local query_fixed = game.getgamename()
+        if apikey and apikey ~= nil and apikey ~= "" then
+            local url = "http://localhost:9696/api/v1/search?query=" .. query_fixed .. "&type=search&limit=20&offset=0&categories=1000&categories=4050"
+            url = url:gsub(" ", "%%20")
+
+            local request = http.get(url, headers)
+            if request and request ~= nil and request ~= "" then
+                local ret_val = {}
+                local request_dec = JsonWrapper.parse(request)
+
+                for _, v in pairs(request_dec) do
+                    local entry = {
+                        name = v.title .. " " .. v.indexer,
+                        links = {}
+                    }
+                    if isMagnetLink(v.guid) then
+                        table.insert(entry.links, { name = "Download", link = v.guid, addtodownloadlist = true })
+                        table.insert(entry.links, { name = "Info", link = v.infoUrl, addtodownloadlist = false })
+                    else
+                        if v.downloadUrl ~= nil and v.downloadUrl ~= "" then
+                            table.insert(entry.links, { name = "Download", link = v.downloadUrl, addtodownloadlist = false })
+                            table.insert(entry.links, { name = "Info", link = v.infoUrl, addtodownloadlist = false })
+                        else
+                            table.insert(entry.links, { name = "Download", link = v.guid, addtodownloadlist = false })
+                            table.insert(entry.links, { name = "Info", link = v.infoUrl, addtodownloadlist = false })
+                        end
+                    end
+
+                    table.insert(ret_val, entry)
+                end
+
+                communication.receiveSearchResults(ret_val)
+            end
+        else
+                Notifications.push_error("Prowlarr Script", "no API key provided in script")
+        end
+    end
+
+    client.add_callback("on_gameselected", prowlarr)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
