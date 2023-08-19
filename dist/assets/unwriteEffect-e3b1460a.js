bc25b06dcb582db6082c38acf5dc4058bccdc6a3
jma <58450502+Y0URD34TH@users.noreply.github.com>
2023-08-19 19:09:23 -0300

    Add files via upload

---
+++ dist/assets/unwriteEffect-e3b1460a.js
@@ -0,0 +1,1 @@
+import{t as l}from"./writeEffect-e46f04ef.js";const c=async(r,n)=>{const e=r.innerHTML.replaceAll("&amp;","&");for(let t=e.length-1;t>=0;t--)e[t]===">"&&(t=e.lastIndexOf("<",t)),r.innerHTML=e.slice(0,t),await l(n.unwriteInterval?n.unwriteInterval:n.interval)};export{c as u};
