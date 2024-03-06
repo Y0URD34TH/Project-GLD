4b159061df89b53bd714d4b123121b7a70739d34
jma <58450502+Y0URD34TH@users.noreply.github.com>
2024-03-06 20:33:09 -0300

    attach-detach-console

---
+++ Scripts/attach-detach-console.lua
@@ -0,0 +1,21 @@
+--to view examples and lua params go in this github page: https://github.com/Y0URD34TH/Project-GLD/blob/main/LuaParams.md
+menu.add_button("Attach Console")
+menu.add_button("Detach Console")
+
+local function Attach()
+utils.AtachConsole() --change to Attach when updated
+end
+
+local function Detach()
+utils.DetachConsole()
+end
+
+client.add_callback("on_button_Attach Console", Attach)
+client.add_callback("on_button_Detach Console", Detach)
+
+
+
+
+
+
+
